2dd66b73f437998761a6e42648881bc6a63221dd
Teje8 <jteruel8@gmail.com>
2022-08-29 21:00:24 -0300

    add get read id

--- routes/cityRoutes.js
+++ routes/cityRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 //const cityController = require('../controllers/cityController') --> aca me traigo todo el objeto/controlador
-const {create} = require('../controllers/cityController') // aca me traigo una propiedad/metodo del objeto/controlador
+const {create, read} = require('../controllers/cityController') // aca me traigo una propiedad/metodo del objeto/controlador
 
 
 router.post('/', create)
+router.get('/:id', read)
 
 module.exports = router;
