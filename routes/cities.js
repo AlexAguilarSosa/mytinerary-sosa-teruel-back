3cd5e51b8bbfcfd299e51087be56c82bc0d97de7
Teje8 <jteruel8@gmail.com>
2022-08-31 18:53:16 -0300

    add routes

--- routes/cities.js
+++ routes/cities.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 //const cityController = require('../controllers/cityController') --> aca me traigo todo el objeto/controlador
-const {create, read, all} = require('../controllers/cityController') // aca me traigo una propiedad/metodo del objeto/controlador
+const {create, read, update, destroy, getAll, filter} = require('../controllers/cityController') // aca me traigo una propiedad/metodo del objeto/controlador
 
 
 //router.metodo('ruta', controlador)
-router.get('/', all)
+
 router.post('/', create)
 router.get('/:id', read)
+router.patch('/:id', update)
+router.delete('/:id', destroy)
+router.get('/', filter)
+router.get('/', getAll)
+
 
 module.exports = router;
