9b62e7b2231a446d8c3a6ae079ca64e27a5abfd9
Teje8 <jteruel8@gmail.com>
2022-08-31 14:15:44 -0300

    fix populate

--- populate.js
+++ populate.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
-const db = require('./config/database') // importamos la conexion a la base de datos
+const DataBase = require('./config/database') // importamos la conexion a la base de datos
 const City = require('./models/City') //importamos los modelos que necesito para las operaciones
 
 City.create({
-    city: '',
-    country: '',
-    photo: '',
-    population: '',
-    foundation: ''
+    city: "Mendoza",
+    country: "Argentina",
+    photo: "http://localhost:4000",
+    population: "1000000",
+    foundation: "1850",
+    currency: "ARS",
+    highseason: "March-August"
 })
