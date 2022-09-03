ba76a05b8e88d42274db8c87ec11c6be21d85528
Alex Sosa <alexaguilar1698@gmail.com>
2022-09-03 13:20:44 -0300

    se agrego enrutador de auth

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const cityRouter = require('./cities')
+const authRouter = require('/auth')
 /* GET home page. */
 //req = request, peticion del cliente
 //res = response, mensaje que envia el servidor
@@ -10,5 +11,6 @@ router.get('/', function(req, res, next) {
 
 //router.metodo('ruta', controlador)
 router.use('/cities', cityRouter)
+router.use('/auth', authRouter)
 
 module.exports = router;
