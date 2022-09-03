903a9e120336d9fc90debf079695af527f23c26e
Alex Sosa <alexaguilar1698@gmail.com>
2022-09-03 13:41:07 -0300

    se corrigio el nombre

--- routes/itinerary.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const {create} = require('../controllers/itineraryController')
-
-router.post('/itineraries', create)
-
-module.exports = router;
