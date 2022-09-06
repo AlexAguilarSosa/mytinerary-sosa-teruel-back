045ef61958f701e6442256a9a3bdc9b65d11ede8
Teje8 <jteruel8@gmail.com>
2022-09-06 14:30:44 -0300

    add cities ref

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
+const City = require('./City')
 
 const commentsSchema = new mongoose.Schema({
     comment: {type: String, required:true},
     user: {type: String, required:false},
-    itinerary: {type: String, required:false}
+    itinerary: {type: String, required:false},
+    city: {type: mongoose.Types.ObjectId, ref: 'cities', required:true}
 })
 
 
